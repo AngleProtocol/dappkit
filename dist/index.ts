d9f86b1fbd3b780a8ba5755ffc5d432f4c78aca0
sheykei <clement4saunier@gmail.com>
2024-10-23 12:16:05 +0200

    revert

--- dist/index.ts
+++
@@ -1,8 +0,0 @@
-import "./style.css";
-export * from "./context/Dapp.context";
-export { default as Button } from "./components/primitives/Button";
-export * from "./utils/tailwind";
-export * from "./theming/coloring";
-
-// Everything under the /components directory will be automatically exported here during the build process.
-export * from "./components"
