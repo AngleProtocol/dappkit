c40748bd25ef8927932cdca5304965a8e2136fe9
indaviande <vianney@double.cat>
2024-11-27 09:49:54 +0100

    change obviously for space grortesk

--- src/utils/tailwind.ts
+++ src/utils/tailwind.ts
@@ -129,7 +129,7 @@ export const generateTailwindConfig = () =>
       "3xl": "clamp(24px,calc(0.4167vw + 1.46484375rem),30px)",
     },
     fontFamily: {
-      title: ['"Obviously", serif'],
+      title: ['"Space Grotesk", serif'],
       text: ['"Mulish", sans-serif'],
       mono: ['"Space Mono", sans-serif'],
     },
