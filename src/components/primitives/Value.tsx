69c9fa927a592fa4080c5aef2dd5a4a9f7933fe1
indaviande <vianney@double.cat>
2024-11-28 14:39:57 +0100

    Hero section

--- src/components/primitives/Value.tsx
+++ src/components/primitives/Value.tsx
@@ -1,26 +1,16 @@
-import { mergeClass } from "dappkit/src";
-import type { Component, Styled } from "dappkit/src";
+import { mergeClass } from "dappkit";
+import { textStyles } from "./Text";
+import type { Component, Styled } from "dappkit";
 import { format } from "numerable";
-import { ReactNode } from "react";
+import type { ReactNode } from "react";
+
 import { tv } from "tailwind-variants";
 
 export const valueStyles = tv({
   base: "text-main-11 font-text font-normal",
   variants: {
-    look: {
-      soft: "text-main-8",
-      base: "text-main-11",
-      bold: "text-secondary-12",
-      tint: "text-accent-12",
-      hype: "text-accent-11",
-    },
-    size: {
-      xs: "text-xs rounded",
-      sm: "text-sm rounded-sm",
-      md: "text-md rounded-md",
-      lg: "text-lg rounded-lg",
-      xl: "text-xl rounded-xl",
-    },
+    look: textStyles.variants.look,
+    size: textStyles.variants.size,
   },
   defaultVariants: {
     size: "md",
@@ -28,8 +18,16 @@ export const valueStyles = tv({
   },
 });
 
-export type ValueFormatProps = { children?: number | string; format: string; value?: boolean, fallback?: (value: number | string) => ReactNode };
-export type ValueProps = Component<Styled<typeof valueStyles> & ValueFormatProps, HTMLDivElement>;
+export type ValueFormatProps = {
+  children?: number | string;
+  format: string;
+  value?: boolean;
+  fallback?: (value: number | string) => ReactNode;
+};
+export type ValueProps = Component<
+  Styled<typeof valueStyles> & ValueFormatProps,
+  HTMLDivElement
+>;
 
 export default function Value({
   look,
@@ -43,11 +41,13 @@ export default function Value({
 }: ValueProps) {
   if (value) return format(children, _format, { currency: "USD" });
   return (
-    <div
+    <p
       className={mergeClass(valueStyles({ size, look }), className)}
       {...props}
     >
-      {children && (fallback?.(children) || format(children, _format, { currency: "USD" }))}
-    </div>
+      {children &&
+        (fallback?.(children) ||
+          format(children, _format, { currency: "USD" }))}
+    </p>
   );
 }
