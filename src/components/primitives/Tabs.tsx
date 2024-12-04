8c28423917c6ad5de0cc7d2e8a4346bdd02b9061
indaviande <vianney@double.cat>
2024-12-04 18:41:00 +0100

    tabs done

--- src/components/primitives/Tabs.tsx
+++ src/components/primitives/Tabs.tsx
@@ -1,49 +1,64 @@
-import { Link } from "@remix-run/react";
+import { Link, useLocation } from "@remix-run/react";
 import { tv, type VariantProps } from "tailwind-variants";
 import { mergeClass } from "../../utils/css";
-import type { Component, Themable } from "../../utils/types";
+import type { Component, Styled, Themable } from "../../utils/types";
 import EventBlocker from "./EventBlocker";
 import useThemableProps from "../../hooks/theming/useThemableProps";
 import Group from "../extenders/Group";
-import { useState, type ReactNode } from "react";
+import type { ReactNode } from "react";
 
 export const tabsStyles = tv(
   {
-    base: "flex items-center ease !leading-none text-xl border-main-0 font-bold select-none focus-visible:outline focus-visible:outline-dashed focus-visible:outline-main-12 text-nowrap font-text",
+    base: "",
+    slots: {
+      base: "flex items-center ease !leading-none text-xl border-main-0 font-bold select-none focus-visible:outline focus-visible:outline-dashed focus-visible:outline-main-12 text-nowrap font-text",
+      line: "",
+      active: "",
+    },
     variants: {
       look: {
         soft: {
           base: "text-main-12",
           line: "border-b-1 border-accent-11",
-          active: "border-b-2 border-main-11 text-main-11",
+          active: "border-accent-11",
         },
         base: {
           base: "text-main-12",
           line: "border-b-1 border-accent-11",
-          active: "border-b-2 border-main-11 text-main-11",
+          active: "border-accent-11",
         },
         bold: {
           base: "text-main-12",
           line: "border-b-1 border-accent-11",
-          active: "border-b-2 border-main-11 text-main-11",
+          active: "border-accent-11",
         },
         tint: {
           base: "text-main-12",
           line: "border-b-1 border-accent-11",
-          active: "border-b-2 border-main-11 text-main-11",
+          active: "border-accent-11",
         },
         hype: {
           base: "text-main-12",
           line: "border-b-1 border-accent-11",
-          active: "border-b-2 border-main-11 text-main-11",
+          active: "border-accent-11",
         },
       },
       size: {
-        xs: "border-b-1 pb-xs gap-sm text-xs",
-        sm: "border-b-2 pb-sm gap-sm text-sm",
-        md: "border-b-4 pb-md gap-sm text-base",
-        lg: "border-b-5 pb-lg gap-sm text-lg",
-        xl: "border-b-6 pb-xl gap-sm text-xl",
+        xs: {
+          base: "border-b-1 pb-xs gap-sm text-xs",
+        },
+        sm: {
+          base: "border-b-2 pb-sm gap-sm text-sm",
+        },
+        md: {
+          base: "border-b-4 pb-md gap-sm text-base",
+        },
+        lg: {
+          base: "border-b-5 pb-lg gap-sm text-lg",
+        },
+        xl: {
+          base: "border-b-6 pb-xl gap-sm text-xl",
+        },
       },
     },
     defaultVariants: {
@@ -59,7 +74,8 @@ type TabsStyleProps = VariantProps<typeof tabsStyles>;
 type TabsType = { label: ReactNode; link: string; key: string }[];
 
 export type TabsProps = Component<
-  TabsStyleProps &
+  Styled<typeof tabsStyles> &
+    TabsStyleProps &
     Themable & {
       to?: string;
       className?: string;
@@ -81,10 +97,7 @@ export default function Tabs({
 }: TabsProps) {
   const themeVars = useThemableProps(props);
   const styleProps = tabsStyles({ look, size });
-
-  const [activeTab, setActiveTab] = useState<string | undefined>(
-    tabs.length > 0 ? tabs[0].key : undefined
-  );
+  const location = useLocation();
 
   const { base, active, line } = tabsStyles({
     look: look ?? "base",
@@ -92,40 +105,44 @@ export default function Tabs({
   });
 
   return (
-    <Group size="xl" className={mergeClass(line(), "items-center w-full")}>
-      {tabs.map((tab) =>
-        tab.link ? (
-          <EventBlocker key={tab.key}>
-            <Link
-              to={tab.link}
-              style={themeVars}
-              className={mergeClass(
-                styleProps,
-                className,
-                activeTab === tab.key ? active() : base()
-              )}
-              onClick={() => setActiveTab(tab.key)}
-            >
-              {tab.label}
-            </Link>
-          </EventBlocker>
-        ) : (
-          <button
-            key={tab.key}
-            style={themeVars}
-            className={mergeClass(
-              styleProps,
-              className,
-              activeTab === tab.key ? active() : base()
+    <Group className={mergeClass(line(), "gap-xl*2 items-center w-full")}>
+      {tabs.map((tab) => {
+        return (
+          <>
+            {tab.link ? (
+              <EventBlocker key={tab.key}>
+                <Link
+                  to={tab.link}
+                  style={themeVars}
+                  className={mergeClass(
+                    styleProps,
+                    base(),
+                    className,
+                    location.pathname === tab.link && active()
+                  )}
+                >
+                  {tab.label}
+                </Link>
+              </EventBlocker>
+            ) : (
+              <button
+                key={tab.key}
+                style={themeVars}
+                className={mergeClass(
+                  styleProps,
+                  base(),
+                  className,
+                  location.pathname === tab.link && active()
+                )}
+                type="button"
+                {...props}
+              >
+                {tab.label}
+              </button>
             )}
-            type="button"
-            onClick={() => setActiveTab(tab.key)}
-            {...props}
-          >
-            {tab.label}
-          </button>
-        )
-      )}
+          </>
+        );
+      })}
     </Group>
   );
 }
