4b39ca231eccb288b9fa44d88e2ad794d203aaa7
sheykei <47207681+clement4saunier@users.noreply.github.com>
2024-12-09 17:55:49 +0100

    fix: time and divider (#20)
    * add: control over divider colors + Connected component
    
    * fix: code quality
    
    * fix: time and divider
    
    * lint

--- src/components/primitives/Divider.tsx
+++ src/components/primitives/Divider.tsx
@@ -44,9 +44,7 @@ export type DividerProps = Component<
 >;
 
 export default function Divider({ vertical = false, horizontal = true, look, className, ...props }: DividerProps) {
-  if (horizontal) return <div className={mergeClass(dividerStyles({ look }), className)} {...props} />;
+  if (horizontal && !vertical) return <div className={mergeClass(dividerStyles({ look }), className)} {...props} />;
 
-  return (
-    <div className={mergeClass(dividerStyles({ look, vertical: vertical ?? !horizontal }), className)} {...props} />
-  );
+  return <div className={mergeClass(dividerStyles({ look, vertical: true }), className)} {...props} />;
 }
