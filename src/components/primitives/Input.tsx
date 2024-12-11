aaf64c4f485e2d37c71d685e45d05e1074d22914
indaviande <vianney@double.cat>
2024-12-11 13:10:37 +0100

    add fitlers

--- src/components/primitives/Input.tsx
+++ src/components/primitives/Input.tsx
@@ -7,22 +7,22 @@ import type { Component, GetSet, Styled } from "../../utils/types";
 import Group from "../extenders/Group";
 
 export const inputStyles = tv({
-  base: "text-main-12 flex items-center   text-nowrap font-text",
+  base: "flex items-center placeholder:text-main-11 text-nowrap font-text",
   variants: {
     look: {
-      none: "bg-main-0 border-0",
-      soft: "bg-main-2 border-main-2 hover:border-main-4 active:border-main-7 hover:text-main-12 focus-within:outline focus-within:outline-main-12",
-      base: "bg-main-6 focus-within:outline focus-within:outline-main-12",
-      bold: "bg-main-3 border-main-4 hover:border-main-4 active:border-main-7 hover:text-main-12 focus-within:outline focus-within:outline-main-12",
-      tint: "bg-main-1 border-accent-6 hover:border-accent-8 active:bg-main-2 text-main-12 focus-within:outline focus-within:outline-main-12",
-      hype: "bg-main-0 border-accent-8 hover:border-accent-10 active:border-accent-8 hover:text-main-12 focus-within:outline focus-within:outline-main-12",
+      none: "text-main-12 bg-main-0 border-0",
+      soft: "text-main-11 bg-main-0 border-main-9 border-1 active:border-main-7 focus-within:outline focus-within:outline-main-12",
+      base: "text-main-12 bg-main-6 focus-within:outline focus-within:outline-main-12",
+      bold: "text-main-12 bg-main-3 border-main-4 hover:border-main-4 active:border-main-7 hover:text-main-12 focus-within:outline focus-within:outline-main-12",
+      tint: "text-main-12 bg-main-1 border-accent-6 hover:border-accent-8 active:bg-main-2 focus-within:outline focus-within:outline-main-12",
+      hype: "text-main-12 bg-main-0 border-accent-8 hover:border-accent-10 active:border-accent-8 hover:text-main-12 focus-within:outline focus-within:outline-main-12",
     },
     size: {
       xs: "px-xs py-xs text-xs rounded-xs",
       sm: "px-sm py-sm text-sm rounded-sm",
-      md: "px-md py-md text-md rounded-md",
-      lg: "px-lg py-lg text-lg rounded-lg",
-      xl: "px-xl py-xl text-3xl rounded-xl",
+      md: "px-md py-sm text-md rounded-md",
+      lg: "px-lg py-md text-lg rounded-lg",
+      xl: "px-xl py-lg text-3xl rounded-xl",
     },
   },
   defaultVariants: {
@@ -31,7 +31,14 @@ export const inputStyles = tv({
   },
 });
 
-export const extensions = ["header", "footer", "prefix", "suffix", "label", "hint"] as const;
+export const extensions = [
+  "header",
+  "footer",
+  "prefix",
+  "suffix",
+  "label",
+  "hint",
+] as const;
 export type InputExtension = (typeof extensions)[number];
 
 export type InputProps<T = string> = Component<
@@ -44,9 +51,16 @@ export type InputProps<T = string> = Component<
 function Input({ look, size, state, className, ...props }: InputProps) {
   const { header, footer, prefix, suffix, label, hint, ...rest } = props;
 
-  if (extensions.some(extension => !!props?.[extension]))
+  if (extensions.some((extension) => !!props?.[extension]))
     return (
-      <label className={mergeClass(inputStyles({ look, size }), className, "flex-col flex")} htmlFor="input">
+      <label
+        className={mergeClass(
+          inputStyles({ look, size }),
+          className,
+          "flex-col flex"
+        )}
+        htmlFor="input"
+      >
         <label htmlFor="input" className="w-full flex">
           {header}
         </label>
@@ -54,9 +68,13 @@ function Input({ look, size, state, className, ...props }: InputProps) {
           {prefix && <label htmlFor="input">{prefix}</label>}
           <input
             id="input"
-            className={mergeClass(inputStyles({ look: "none", size }), className, "w-full !flex-1 !px-0 !py-0")}
+            className={mergeClass(
+              inputStyles({ look: "none", size }),
+              className,
+              "w-full !flex-1 !px-0 !py-0"
+            )}
             value={state?.[0]}
-            onChange={e => state?.[1]?.(e?.target?.value)}
+            onChange={(e) => state?.[1]?.(e?.target?.value)}
             {...rest}
           />
           {suffix && <label htmlFor="input">{suffix}</label>}
@@ -70,13 +88,17 @@ function Input({ look, size, state, className, ...props }: InputProps) {
     <input
       className={mergeClass(inputStyles({ look, size }), className)}
       value={state?.[0]}
-      onChange={e => state?.[1]?.(e?.target?.value)}
+      onChange={(e) => state?.[1]?.(e?.target?.value)}
       {...rest}
     />
   );
 }
 
-Input.BigInt = function InputBigInt({ state, base, ...props }: InputProps<bigint> & { base: number }) {
+Input.BigInt = function InputBigInt({
+  state,
+  base,
+  ...props
+}: InputProps<bigint> & { base: number }) {
   const [internal, setInternal] = useState<bigint>();
   const [displayed, setDisplayed] = useState("0.0");
   const [_getter, setter] = state ?? [];
@@ -98,7 +120,8 @@ Input.BigInt = function InputBigInt({ state, base, ...props }: InputProps<bigint
 
         const raw = v?.replaceAll(",", "") ?? "";
         const isInputtingDecimals =
-          v?.split(".")?.[1]?.[v?.split?.(".")?.[1]?.length - 1] === "0" || v?.[v.length - 1] === ".";
+          v?.split(".")?.[1]?.[v?.split?.(".")?.[1]?.length - 1] === "0" ||
+          v?.[v.length - 1] === ".";
         const transformed = parseUnits(raw, base);
 
         if (raw === "0") setDisplayed("");
@@ -107,11 +130,16 @@ Input.BigInt = function InputBigInt({ state, base, ...props }: InputProps<bigint
         setter?.(transformed) ?? setInternal(transformed);
       } catch (_err) {}
     },
-    [setter, base],
+    [setter, base]
   );
 
   //TODO: implement setter callback
-  return <Input state={[displayed, v => typeof v !== "function" && setValue(v)]} {...props} />;
+  return (
+    <Input
+      state={[displayed, (v) => typeof v !== "function" && setValue(v)]}
+      {...props}
+    />
+  );
 };
 
 export default Input;
