f3d8de72144a7fe3b5eb4885bc64336e418c8aa2
Hugo Lextrait <hugo.lextrait@gmail.com>
2024-12-16 12:54:46 +0100

    Sort wallets and add icons for walletconnect and coinbase

--- src/components/dapp/WalletConnectors.tsx
+++ src/components/dapp/WalletConnectors.tsx
@@ -6,24 +6,52 @@ import Icon from "../primitives/Icon";
 import Image from "../primitives/Image";
 import Input from "../primitives/Input";
 import Text from "../primitives/Text";
+import type { Connector } from "wagmi";
+import walletConnectIcon from "../../assets/walletConnect.svg";
+import walletCoinbaseIcon from "../../assets/walletCoinbase.svg";
+import { useMemo } from "react";
 
 export default function WalletConnectors() {
   const { config, connect, connector: _connected } = useWalletContext();
 
+  const sortedConnectors = useMemo(
+    () =>
+      [...config.connectors].sort((a, b) => {
+        const priority = (connector: Connector) => {
+          switch (connector.name.toLowerCase()) {
+            case "metamask":
+              return 0;
+            case "rabby wallet":
+              return 1;
+            case "walletconnect":
+              return 2;
+            default:
+              return 3;
+          }
+        };
+        return priority(a) - priority(b);
+      }),
+    [config.connectors],
+  );
+
   return (
     <Group className="flex-col w-full">
       <div className="grid grid-flow-row gap-lg">
-        {config.connectors.map(connector => {
+        {sortedConnectors.map(connector => {
           return (
             <Button
               look="base"
-              // look={connected?.id === connector.id ? "hype" : "bold"}
               onClick={() => connect(connector.id)}
               size="xl"
               bold
               className="gap-sm*2"
               key={connector.id}>
-              <Image className="h-lg*2 w-lg*2 rounded-md" alt={connector.name} src={connector.icon} fallback="" />
+              <Image
+                className="h-lg*2 w-lg*2 rounded-full overflow-hidden"
+                alt={connector.name}
+                src={overrideIcons(connector)}
+                fallback=""
+              />
               {connector.name}
               <Icon remix="RiArrowRightUpLine" />
             </Button>
@@ -38,3 +66,14 @@ export default function WalletConnectors() {
     </Group>
   );
 }
+
+const overrideIcons = (connector: Connector) => {
+  switch (connector.name.toLowerCase()) {
+    case "coinbase wallet":
+      return walletCoinbaseIcon;
+    case "walletconnect":
+      return walletConnectIcon;
+    default:
+      return connector.icon;
+  }
+};
