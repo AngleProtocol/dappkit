0c1f4f77fe188e385d4b5cac14f017b87c3237d2
picodes <picodes@angle.money>
2025-01-03 19:10:13 +0100

    chore: lint

--- src/components/dapp/Connected.tsx
+++ src/components/dapp/Connected.tsx
@@ -10,7 +10,10 @@ export default function Connected({ hideSpyMode = false, children, ...props }: C
 
   if (!connected)
     return (
-      <Modal title="Connect Wallet" className="mx-auto w-full max-w-[500px]" modal={<WalletConnectors hideSpyMode={hideSpyMode} />}>
+      <Modal
+        title="Connect Wallet"
+        className="mx-auto w-full max-w-[500px]"
+        modal={<WalletConnectors hideSpyMode={hideSpyMode} />}>
         <Button look="hype" size="md" {...props}>
           Connect
         </Button>
