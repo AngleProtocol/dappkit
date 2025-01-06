a676c8db3418db3d9e7ffb9c2dc9412bf0a4ac66
sheykei <clement4saunier@gmail.com>
2025-01-06 11:15:58 +0100

    add: tx-check

--- src/components/dapp/TransactionButton.tsx
+++ src/components/dapp/TransactionButton.tsx
@@ -5,9 +5,12 @@ import { Hash } from "../..";
 import { useWalletContext } from "../../context/Wallet.context";
 import Button, { type ButtonProps } from "../primitives/Button";
 import Icon from "../primitives/Icon";
+import List from "../primitives/List";
 import { Notifier } from "../primitives/Notifications";
+import Tooltip from "../primitives/Tooltip";
 
 export type TransactionButtonProps = ButtonProps & {
+  enableSponsorCheckbox?: boolean;
   tx?: Parameters<UseSendTransactionReturnType<ResolvedRegister["config"], unknown>["sendTransaction"]>["0"];
   name?: ReactNode;
   onExecute?: (hash: string) => void;
@@ -21,12 +24,13 @@ export default function TransactionButton({
   children,
   onExecute,
   onSuccess,
+  enableSponsorCheckbox,
   onError,
   ...props
 }: TransactionButtonProps) {
   const sendTxHook = useSendTransaction();
   const { status } = sendTxHook;
-  const { address: user, client, sendTransaction } = useWalletContext();
+  const { address: user, chainId, client, sendTransaction, sponsorTransactions, setSponsorTransactions } = useWalletContext();
   const execute = useCallback(async () => {
     if (!tx || !user || !client) return;
 
@@ -119,6 +123,23 @@ export default function TransactionButton({
     }
   }, [tx, client, user, sendTransaction, onExecute, onSuccess, onError, name]);
 
+  //TODO: remove hardcoded chainId check in favor of more integrated and generic implem
+  if (enableSponsorCheckbox && chainId === 324)
+    return (
+      <List flex="row" {...{size: props.size, look: props.look}}>
+        <Button {...props} onClick={execute}>
+          {children}
+          {status === "pending" && <Icon className="animate-spin" remix="RiLoader2Line" />}
+        </Button>
+        {
+          <Button onClick={() => setSponsorTransactions(s => !s)} look="hype">
+            <Tooltip icon={false} helper={<>Enable sponsoring of the transaction</>}>
+              <Icon remix={sponsorTransactions ? "RiCheckboxCircleLine" : "RiCheckboxBlankCircleLine"} />
+            </Tooltip>
+          </Button>
+        }
+      </List>
+    );
   return (
     <Button {...props} onClick={execute}>
       {children}
