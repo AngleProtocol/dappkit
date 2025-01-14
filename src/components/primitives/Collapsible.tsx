aea42cfdec1f98a64e12c5c9d5e3ad4cce33fb0b
Hugo Lextrait <hugo.lextrait@gmail.com>
2025-01-14 19:30:35 +0100

    add classname on collapsible

--- src/components/primitives/Collapsible.tsx
+++ src/components/primitives/Collapsible.tsx
@@ -1,13 +1,17 @@
 import * as RadixCollapsible from "@radix-ui/react-collapsible";
 import type { PropsWithChildren } from "react";
+import { mergeClass } from "../../utils/css";
 import type { GetSet } from "../../utils/types";
 import styles from "./Collapsible.module.css";
 
-export type CollapsibleProps = PropsWithChildren<{ state: GetSet<boolean> }>;
+export type CollapsibleProps = PropsWithChildren<{ state: GetSet<boolean>; className?: string }>;
 
-export default function Collapsible({ state, children }: CollapsibleProps) {
+export default function Collapsible({ state, children, className }: CollapsibleProps) {
   return (
-    <RadixCollapsible.Root className="overflow-hidden" open={state?.[0]} onOpenChange={state?.[1]}>
+    <RadixCollapsible.Root
+      className={mergeClass("overflow-hidden", className)}
+      open={state?.[0]}
+      onOpenChange={state?.[1]}>
       <RadixCollapsible.Content className={styles.animated}>{children}</RadixCollapsible.Content>
     </RadixCollapsible.Root>
   );
