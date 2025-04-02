75d7bb4a8da3cd8fb895c3ba7b10735af45cd309
sheykei <clement4saunier@gmail.com>
2025-04-02 11:23:45 +0200

    fix

--- src/components/extenders/Card.tsx
+++ src/components/extenders/Card.tsx
@@ -1,4 +1,4 @@
-import { Link } from "@remix-run/react";
+import { Link } from "react-router";
 import { tv } from "tailwind-variants";
 import useThemedVariables from "../../hooks/theming/useThemedVariables";
 import { mergeClass } from "../../utils/css";
