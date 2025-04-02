82342eafd80a5d965e2aac708f3bd5b6e3c59cdf
sheykei <clement4saunier@gmail.com>
2025-04-02 11:23:45 +0200

    fix

--- src/components/primitives/Button.tsx
+++ src/components/primitives/Button.tsx
@@ -1,4 +1,4 @@
-import { Link } from "@remix-run/react";
+import { Link } from "react-router";
 import { type VariantProps, tv } from "tailwind-variants";
 import useThemableProps from "../../hooks/theming/useThemableProps";
 import { mergeClass } from "../../utils/css";
